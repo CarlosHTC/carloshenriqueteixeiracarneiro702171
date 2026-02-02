1c315eedc231ccc0aefa80d2d7d77ba2b7a69a52
Carlos Henrique <chtcarneiro@gmail.com>
2026-02-02 00:52:23 -0400

    chore(config): ajustar properties e docker

--- apimusicmanagement/src/main/java/br/com/avaliacao/apimusicmanagement/ApimusicmanagementApplication.java
+++ apimusicmanagement/src/main/java/br/com/avaliacao/apimusicmanagement/ApimusicmanagementApplication.java
@@ -1,9 +1,13 @@
 package br.com.avaliacao.apimusicmanagement;
 
+import br.com.avaliacao.apimusicmanagement.config.integration.RegionalIntegrationProperties;
+import br.com.avaliacao.apimusicmanagement.config.security.AppSecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({AppSecurityProperties.class, RegionalIntegrationProperties.class})
 public class ApimusicmanagementApplication {
 
     public static void main(String[] args) {
